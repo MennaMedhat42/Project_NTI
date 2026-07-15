4cfe19976787df3d0f5652b2194bfcb5f779e4e2
Menna Medhat <mennamedhatascii157@gmail.com>
2026-07-16 02:05:23 +0300

    Delete Patient directory

--- Patient/js/navbar.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function () {
-  let profile = JSON.parse(localStorage.getItem("profile"));
-  if (profile) {
-    document.getElementById("headerName").textContent = profile.name;
-    document.getElementById("headerImage").src = profile.image;
-  }
-});
